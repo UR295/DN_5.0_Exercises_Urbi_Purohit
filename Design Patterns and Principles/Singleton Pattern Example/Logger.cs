31577495648c8e01873e8a591c3bbf57066703f8
Urbi <158612655+UR295@users.noreply.github.com>
2026-06-29 22:18:06 +0530

    Convert Logger class
    Refactor Logger class to C# from Java, updating method names and syntax.

---
+++ Design Patterns and Principles/Singleton Pattern Example/Logger.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace SingletonPatternExample
+{
+    class Logger
+    {
+        private static Logger instance;
+        private Logger()
+        {
+        }
+        public static Logger GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Logger();
+            }
+
+            return instance;
+        }
+        public void Log(string message)
+        {
+            Console.WriteLine("Log: " + message);
+        }
+    }
+}
